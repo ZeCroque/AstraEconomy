03939fed00272b40bb385462e57bf67db7851bf0
ZeCroque <zobilezeub@gmail.com>
2026-08-01 20:01:24 +0200

    Added warning for missing thumbnail

--- build.py
+++ build.py
@@ -1,15 +1,17 @@
 from CIScripts import translator
 from CIScripts import ba2_creator
 from CIScripts import utils
+from CIScripts import mod_info
+import os
 
 def main():   
-    #TODO test files, warn no thumbnail, etc..
-
     if utils.AskForUserConfirm("Would you like to localize the .esm?"):
         translator.Translate()
         if(not utils.AskForUserConfirm(".esm localized. Proceed to archive creation?")):
             return
-    ba2_creator.CreateArchives()
+
+    if(os.path.isfile(mod_info.config.modShortName + "_Thumbnail.png") or utils.AskForUserConfirm("Thumbnail file not found. Proceed anyway?")):
+        ba2_creator.CreateArchives()
 
 if __name__ == "__main__":
     main()
